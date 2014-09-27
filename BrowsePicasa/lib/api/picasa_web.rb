707d753fcb396a23b9ec1240eb5a8809f24c162f
Sny <sny.aggarwal@gmail.com>
2014-09-27 14:38:58 +0800

    picasa_web api abstraction and specs

--- BrowsePicasa/lib/api/picasa_web.rb
+++ BrowsePicasa/lib/api/picasa_web.rb
@@ -5,48 +5,52 @@ module PicasaWeb
   $user_data = 'http://picasaweb.google.com/data/feed/api/user/'
   $profile_url = 'https://www.googleapis.com/plus/v1/people/me?access_token='
   $revoke_url = 'https://accounts.google.com/o/oauth2/revoke?token='
-  $albums_url = 'https://picasaweb.google.com/data/feed/api/user/'
+  $albums_url = 'https://picasaweb.google.com/data/feed/api/user/%{user_id}?kind=album&access_token=%{access_token}'
   $photos_url = "https://picasaweb.google.com/data/feed/api/user/%{user_id}/albumid/%{album_id}?max-results=%{max_limit}&kind=photo&access_token=%{access_token}"
   $post_comments_url = "https://picasaweb.google.com/data/feed/api/user/%{user_id}/albumid/%{album_id}/photoid/%{photo_id}?kind=comment&access_token=%{access_token}"
 
   def load_albums(user_id, access_token)
-    res = open($albums_url + user_id + '?kind=album&access_token=' + access_token)
-    Hash.from_xml(res.read)
+    get_xml_and_return_hash_for($albums_url % {access_token: access_token, user_id: user_id})
   end
 
   def load_photos(access_token, user_id, album_id, limit)
-    photos_end_point = $photos_url % { user_id: user_id, album_id: album_id, max_limit: limit, access_token: access_token }
-    res = open(photos_end_point)
-    Hash.from_xml(res.read)
+    get_xml_and_return_hash_for($photos_url % { user_id: user_id, album_id: album_id, max_limit: limit, access_token: access_token })
   end
 
-  def profile(token, headers={})
-    end_point = URI.parse($profile_url + token)
-    request = Net::HTTP.new(end_point.host, end_point.port)
-    request.use_ssl = true
-    res = request.get(end_point.request_uri)
-    log_response(end_point, res)
-    JSON.parse(res.body)
+  def profile(token)
+    get_json_for($profile_url + token)
   end
+
 
   def disconnect(token)
-    end_point = URI.parse($revoke_url + token)
-    request = Net::HTTP.new(end_point.host, end_point.port)
-    request.use_ssl = true
-    res = request.get(end_point.request_uri)
-    log_response(end_point, res)
-    res.code
+    res = open($revoke_url + token)
+    res.status[0]
   end
 
-  def post_comment(comment, params)
+  def post_comment(params)
+    comment = "<entry xmlns='http://www.w3.org/2005/Atom'> <content>%{comment}</content> <category scheme='http://schemas.google.com/g/2005#kind' term='http://schemas.google.com/photos/2007#comment'/></entry>" % params
+
     comments_end_point = URI.parse($post_comments_url % params)
-
-    comment = "<entry xmlns='http://www.w3.org/2005/Atom'> <content>%{comment}</content> <category scheme='http://schemas.google.com/g/2005#kind' term='http://schemas.google.com/photos/2007#comment'/> </entry>" % comment
-
-    resp, data = Net::HTTP.post_form(comments_end_point, comment)
+    http = Net::HTTP.new(comments_end_point.host, comments_end_point.port)
+    response = http.start do |http|
+      req = Net::HTTP::Post.new(comments_end_point.request_uri)
+      req.body = comment
+      http.request(req)
+    end
+    response
   end
 
   private
+  def get_json_for(url)
+    res = open(url)
+    JSON.parse(res.read)
+  end
+
+  def get_xml_and_return_hash_for(end_point)
+    res = open(end_point)
+    Hash.from_xml(res.read)
+  end
+
   def log_response(request, response)
     if has_error_status_code(response)
       if Rails.logger
---
+++ BrowsePicasa/spec/lib/picasa_web_spec.rb
@@ -0,0 +1,65 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+class Dummy
+  include PicasaWeb
+end
+
+describe 'PicasaWeb' do
+  before(:each) do
+    @dummy_class = Dummy.new
+  end
+
+  describe 'HTTP get, post and delete' do
+    let(:token) { 'access_token' }
+    let(:mock_http) { double('http') }
+    let(:http_response) { double 'Net::HTTPResponse' }
+
+    it 'should call Net/HTTP get services on profile' do
+      expect(http_response).to receive(:read).and_return('{ "photo": [ { "id": 1 } ] }')
+      expect(@dummy_class).to receive(:open).with('https://www.googleapis.com/plus/v1/people/me?access_token=access_token').and_return(http_response)
+
+      response = @dummy_class.profile(token)
+
+      expect(response).to eq({ "photo" => [{ "id" => 1 }] })
+    end
+
+    it 'should call Net/HTTP get services on disconnect' do
+      expect(http_response).to receive(:status).and_return(['200', 'OK'])
+      expect(@dummy_class).to receive(:open).with('https://accounts.google.com/o/oauth2/revoke?token=access_token').and_return(http_response)
+
+      response = @dummy_class.disconnect(token)
+
+      expect(response).to eq('200')
+    end
+
+    it 'should convert xml to hash on album load' do
+      expect(http_response).to receive(:read).and_return("<id>1</id>")
+      expect(@dummy_class).to receive(:open).with('https://picasaweb.google.com/data/feed/api/user/user_id?kind=album&access_token=access_token').and_return(http_response)
+
+      response = @dummy_class.load_albums('user_id', token)
+
+      expect(response).to eq({ 'id' => "1" })
+    end
+
+    it 'should call Net/HTTP post services on comment post' do
+      mock_uri = double('uri', host: 'picasaweb.google.com', port: '80', request_uri: 'https://picasaweb.google.com/data/feed/api/user/user_id/albumid/album_id/photoid/photo_id?kind=comment&access_token=%access_token')
+
+      mock_response = double(Net::HTTPResponse)
+
+      expect(URI).to receive(:parse)
+                     .with('https://picasaweb.google.com/data/feed/api/user/user_id/albumid/album_id/photoid/photo_id?kind=comment&access_token=token')
+                     .and_return(mock_uri)
+      expect(Net::HTTP).to receive(:new).with(mock_uri.host, mock_uri.port).and_return(mock_http)
+      expect(Net::HTTP::Post).to receive(:new).with(mock_uri.request_uri).and_return(mock_response)
+      expect(mock_response).to receive(:body=).with("<entry xmlns='http://www.w3.org/2005/Atom'> <content>comment</content> <category scheme='http://schemas.google.com/g/2005#kind' term='http://schemas.google.com/photos/2007#comment'/></entry>")
+      expect(mock_http).to receive(:start).and_yield(mock_http)
+      expect(mock_http).to receive(:request).with(mock_response).and_return(http_response)
+
+
+      response = @dummy_class.post_comment({ comment: 'comment', access_token: 'token', user_id: 'user_id', album_id: 'album_id', photo_id: 'photo_id' })
+
+      expect(response).to eq(http_response)
+    end
+  end
+end
