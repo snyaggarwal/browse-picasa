32a05f586c005cb344261130d21bf934e7f3700f
Sny <sny.aggarwal@gmail.com>
2014-09-29 15:17:03 +0800

    dead code :-/

--- BrowsePicasa/app/assets/javascripts/angular/services/Albums.js
+++ BrowsePicasa/app/assets/javascripts/angular/services/Albums.js
@@ -1,18 +1,10 @@
 BrowsePicasa.factory('Albums', ['$http', function($http) {
-  var photosList;
-
   return {
     all: function() {
       return $http.get('/albums');
     },
     photos: function(albumId) {
       return $http.get("/albums/" + albumId + '/photos');
-    },
-    setPhotos: function(photos) {
-      photosList = photos;
-    },
-    getPhotos: function() {
-      return photosList;
     }
   }
 }]);
